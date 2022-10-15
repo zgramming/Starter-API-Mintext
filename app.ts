e215cb93ad52e37a7d7985fff8fb309b584aedda
Zeffry Reynando <zeffry.reynando@gmail.com>
2022-10-15 13:37:05 +0700

    Rework app.ts

--- app.ts
+++ app.ts
@@ -1,24 +1,45 @@
-import Koa from "koa";
-import Logger from "koa-logger";
-import Json from "koa-json";
-import BodyParser from "koa-bodyparser";
-import KoaBody from "koa-body";
-import router from "./routes/routes";
+import Koa from 'koa';
+import KoaBody from 'koa-body';
+import BodyParser from 'koa-bodyparser';
+import KoaCompose from 'koa-compose';
+import Json from 'koa-json';
+import Logger from 'koa-logger';
+
+import AccessMenuRouter from './routes/setting/access_menu';
+import AccessModulRouter from './routes/setting/access_modul';
+import DocumentationRouter from './routes/setting/documentation';
+import MasterCategoryRouter from './routes/setting/master_category';
+import MasterDataRouter from './routes/setting/master_data';
+import MenuRouter from './routes/setting/menu';
+import ModulRouter from './routes/setting/modul';
+import ParameterRouter from './routes/setting/parameter';
+import UserRouter from './routes/setting/user';
+import UserGroupRouter from './routes/setting/user_group';
 
 const cors = require("@koa/cors");
 const app = new Koa();
-
 app.use(
   KoaBody({
     multipart: true,
   })
 );
+
 app.use(cors());
 app.use(Json());
 app.use(Logger());
 app.use(BodyParser());
-app.use(router());
 
-app.listen(3000, () => {
-  console.log("Koa server is started");
+app.use(KoaCompose([UserRouter.routes(), UserRouter.allowedMethods()]));
+app.use(KoaCompose([UserGroupRouter.routes(), UserGroupRouter.allowedMethods()]));
+app.use(KoaCompose([ModulRouter.routes(), ModulRouter.allowedMethods()]));
+app.use(KoaCompose([MenuRouter.routes(), MenuRouter.allowedMethods()]));
+app.use(KoaCompose([AccessModulRouter.routes(), AccessModulRouter.allowedMethods()]));
+app.use(KoaCompose([AccessMenuRouter.routes(), AccessMenuRouter.allowedMethods()]));
+app.use(KoaCompose([MasterCategoryRouter.routes(), MasterCategoryRouter.allowedMethods()]));
+app.use(KoaCompose([MasterDataRouter.routes(), MasterDataRouter.allowedMethods()]));
+app.use(KoaCompose([ParameterRouter.routes(), ParameterRouter.allowedMethods()]));
+app.use(KoaCompose([DocumentationRouter.routes(), DocumentationRouter.allowedMethods()]));
+
+app.listen(process.env.PORT, () => {
+  console.log("Koa server is started on " + process.env.PORT);
 });
